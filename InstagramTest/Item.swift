7a9675104c2049a663fc8b14dad3d19906db9447
Guillaume <guillaume-genest2@hotmail.fr>
2025-03-08 14:06:25 +0100

    Initial Commit

---
+++ InstagramTest/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  InstagramTest
+//
+//  Created by Guillaume on 08/03/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
